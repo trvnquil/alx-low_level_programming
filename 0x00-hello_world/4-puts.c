50d44c6598c920c74c8e842298672801591ef259
trvnquil <iamtranquil02@gmail.com>
2022-12-24 13:44:42 -0800

    Updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints'Programming is like building a multilingual puzzle'
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
